f6e2dd5b061eeb22ef0db0cabeb8b006ccad6d04
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-01 16:36:47 -0500

    created routes for home page and create page

--- app.py
+++ app.py
@@ -19,3 +19,45 @@ mongo = PyMongo(app)
 ############################################################
 # ROUTES
 ############################################################
+
+@app.route('/')
+def plants_list():
+    """Display the plants list page."""
+
+    # TODO: Replace the following line with a database call to retrieve *all*
+    # plants from the Mongo database's `plants` collection.
+    plants_data = mongo.db.plants_data.find()
+    context = {
+        'plants': plants_data,
+    }
+    return render_template('plants_list.html', **context)
+
+@app.route('/about')
+def about():
+    """Display the about page."""
+    return render_template('about.html')
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    """Display the plant creation page & process data from the creation form."""
+    if request.method == 'POST':
+        name = request.form['plant_name']
+        variety = request.form['variety']
+        photo = request.form['photo']
+        date = request.form['date_planted']
+      
+        new_plant = {
+            'name': name,
+            'variety': variety,
+            'photo_url': photo,
+            'date_planted': date
+        }
+        id = mongo.db.plants_data.insert_one(new_plant)
+        return redirect(url_for('detail', plant_id=id))
+
+    else:
+        return render_template('create.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 3000)
