ae8e02e56bd951a18a5ee5c222e6672c99e662be
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-01 16:05:06 -0500

    setup

--- app.py
+++ app.py
@@ -6,3 +6,16 @@
 from flask import Flask, request, redirect, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+
+############################################################
+# SETUP
+############################################################
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
+mongo = PyMongo(app)
+
+############################################################
+# ROUTES
+############################################################
