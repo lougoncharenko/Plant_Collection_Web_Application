c2dc6a18528cf3801018b62d278d029cc2a856a6
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-03 11:58:16 -0500

    included a insert_one in harvest route

--- app.py
+++ app.py
@@ -58,6 +58,44 @@ def create():
     else:
         return render_template('create.html')
 
+@app.route('/plant/<plant_id>')
+def detail(plant_id):
+    """Display the plant detail page & process data from the harvest form."""
+    # TODO: Replace the following line with a database call to retrieve *one*
+    # plant from the database, whose id matches the id passed in via the URL.
+    plant_to_show = mongo.db.plants_data.find_one(plant_id)
+
+    # TODO: Use the `find` database operation to find all harvests for the
+    # plant's id.
+    # HINT: This query should be on the `harvests` collection, not the `plants`
+    # collection.
+    harvests = plant_to_show.find()
+
+    context = {
+        'plant' : plant_to_show,
+        'harvests': harvests
+    }
+    return render_template('detail.html', **context)
+
+
+@app.route('/harvest/<plant_id>', methods=['POST'])
+def harvest(plant_id):
+    """
+    Accepts a POST request with data for 1 harvest and inserts into database.
+    """
+
+    # TODO: Create a new harvest object by passing in the form data from the
+    # detail page form.
+    new_harvest = {
+        'quantity': '', # e.g. '3 tomatoes'
+        'date': '',
+        'plant_id': plant_id
+    }
+
+    mongo.db.harvests_data.insert_one(new_harvest)
+
+    return redirect(url_for('detail', plant_id=plant_id))
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
